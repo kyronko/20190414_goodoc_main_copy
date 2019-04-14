c51c32d68c849bd08be89307118de1e38ccab193
user <kyronko@naver.com>
2019-04-14 14:37:52 +0900

    연습문제/ 응용문제까지 작성

--- app/src/main/java/com/tjedit/a20190414_goodoc_main_copy/MainActivity.java
+++ app/src/main/java/com/tjedit/a20190414_goodoc_main_copy/MainActivity.java
@@ -2,12 +2,38 @@ package com.tjedit.a20190414_goodoc_main_copy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText TextId;
+    Button TextIdBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextId = findViewById(R.id.TextId);
+        TextIdBtn = findViewById(R.id.TextIdBtn);
+
+        TextIdBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String TxtID = TextId.getText().toString();
+
+
+//                Toast.makeText(MainActivity.this, TxtID, Toast.LENGTH_SHORT).show();
+                String toastMessage = String.format("[%s]을 검색합니다.",TxtID);
+               Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
     }
 }
